f32f70962fe074728059ffab390d2c62b4c37a18
Gökçe Aktuğ <gokceeaktug@gmail.com>
2021-01-16 07:02:58 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,287 @@
+//number 8 
+document.getElementById("0").addEventListener("click", function() {
+  alert("A8");
+ 
+});
+document.getElementById("1").addEventListener("click", function() {
+  alert("B8");
+  
+});
+document.getElementById("2").addEventListener("click", function() {
+  alert("C8");
+  
+});
+document.getElementById("3").addEventListener("click", function() {
+  alert("D8");
+  
+});
+
+document.getElementById("4").addEventListener("click", function() {
+  alert("E8");
+ 
+});
+document.getElementById("5").addEventListener("click", function() {
+  alert("F8");
+  
+});
+document.getElementById("6").addEventListener("click", function() {
+  alert("G8");
+  
+});
+document.getElementById("7").addEventListener("click", function() {
+  alert("H8");
+  
+});
+
+//number 7
+
+document.getElementById("8").addEventListener("click", function() {
+  alert("A7");
+ 
+});
+document.getElementById("9").addEventListener("click", function() {
+  alert("B7");
+  
+});
+document.getElementById("10").addEventListener("click", function() {
+  alert("C7");
+  
+});
+document.getElementById("11").addEventListener("click", function() {
+  alert("D7");
+  
+});
+
+document.getElementById("12").addEventListener("click", function() {
+  alert("E7");
+ 
+});
+document.getElementById("13").addEventListener("click", function() {
+  alert("F7");
+  
+});
+document.getElementById("14").addEventListener("click", function() {
+  alert("G7");
+  
+});
+document.getElementById("15").addEventListener("click", function() {
+  alert("H7");
+  
+});
+//number 6
+
+
+document.getElementById("16").addEventListener("click", function() {
+  alert("A6");
+ 
+});
+document.getElementById("17").addEventListener("click", function() {
+  alert("B6");
+  
+});
+document.getElementById("18").addEventListener("click", function() {
+  alert("C6");
+  
+});
+document.getElementById("19").addEventListener("click", function() {
+  alert("D6");
+  
+});
+
+document.getElementById("20").addEventListener("click", function() {
+  alert("E6");
+ 
+});
+document.getElementById("21").addEventListener("click", function() {
+  alert("F6");
+  
+});
+document.getElementById("22").addEventListener("click", function() {
+  alert("G6");
+  
+});
+document.getElementById("23").addEventListener("click", function() {
+  alert("H6");
+  
+});
+//number 5
+
+
+document.getElementById("24").addEventListener("click", function() {
+  alert("A5");
+ 
+});
+document.getElementById("25").addEventListener("click", function() {
+  alert("B5");
+  
+});
+document.getElementById("26").addEventListener("click", function() {
+  alert("C5");
+  
+});
+document.getElementById("27").addEventListener("click", function() {
+  alert("D5");
+  
+});
+
+document.getElementById("28").addEventListener("click", function() {
+  alert("E5");
+ 
+});
+document.getElementById("28").addEventListener("click", function() {
+  alert("F5");
+  
+});
+document.getElementById("30").addEventListener("click", function() {
+  alert("G5");
+  
+});
+document.getElementById("31").addEventListener("click", function() {
+  alert("H5");
+  
+});
+//number 4
+
+
+document.getElementById("32").addEventListener("click", function() {
+  alert("A4");
+ 
+});
+document.getElementById("33").addEventListener("click", function() {
+  alert("B4");
+});
+document.getElementById("34").addEventListener("click", function() {
+  alert("C4");
+  
+});
+document.getElementById("35").addEventListener("click", function() {
+  alert("D4");
+  
+});
+
+document.getElementById("36").addEventListener("click", function() {
+  alert("E4");
+ 
+});
+document.getElementById("37").addEventListener("click", function() {
+  alert("F4");
+  
+});
+document.getElementById("38").addEventListener("click", function() {
+  alert("G4");
+  
+});
+document.getElementById("39").addEventListener("click", function() {
+  alert("H4");
+  
+});
+
+//number 3
+
+document.getElementById("40").addEventListener("click", function() {
+  alert("A3");
+ 
+});
+document.getElementById("41").addEventListener("click", function() {
+  alert("B3");
+  
+});
+document.getElementById("42").addEventListener("click", function() {
+  alert("C3");
+  
+});
+document.getElementById("43").addEventListener("click", function() {
+  alert("D3");
+  
+});
+
+document.getElementById("44").addEventListener("click", function() {
+  alert("E3");
+ 
+});
+document.getElementById("45").addEventListener("click", function() {
+  alert("F3");
+});
+document.getElementById("46").addEventListener("click", function() {
+  alert("G3");
+  
+});
+document.getElementById("47").addEventListener("click", function() {
+  alert("H3");
+  
+});
+
+
+//number 2
+document.getElementById("48").addEventListener("click", function() {
+  alert("A2");
+ 
+});
+document.getElementById("49").addEventListener("click", function() {
+  alert("B2");
+  
+});
+document.getElementById("50").addEventListener("click", function() {
+  alert("C2");
+  
+});
+document.getElementById("51").addEventListener("click", function() {
+  alert("D2");
+  
+});
+
+document.getElementById("52").addEventListener("click", function() {
+  alert("E2");
+ 
+});
+document.getElementById("53").addEventListener("click", function() {
+  alert("F2");
+  
+});
+document.getElementById("54").addEventListener("click", function() {
+  alert("G2");
+  
+});
+document.getElementById("55").addEventListener("click", function() {
+  alert("H2");
+  
+});
+
+
+//number 1
+
+document.getElementById("56").addEventListener("click", function() {
+  alert("A1");
+ 
+});
+document.getElementById("57").addEventListener("click", function() {
+  alert("B1");
+  
+});
+document.getElementById("58").addEventListener("click", function() {
+  alert("C1");
+  
+});
+document.getElementById("59").addEventListener("click", function() {
+  alert("D1");
+  
+});
+
+document.getElementById("60").addEventListener("click", function() {
+  alert("E1");
+ 
+});
+document.getElementById("61").addEventListener("click", function() {
+  alert("F1");
+  
+});
+document.getElementById("62").addEventListener("click", function() {
+  alert("G1");
+  
+});
+document.getElementById("63").addEventListener("click", function() {
+  alert("H1");
+  
+});
+
+
